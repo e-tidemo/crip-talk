3c20a5941c36df26baf4fff57755656e2141363f
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-18 11:25:06 +0100

    Install Django summernote and add more functions to admin page using Summernote

--- forumproject/urls.py
+++ forumproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("forumapp.urls"), name="forumapp-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("forumapp.urls"), name="forumapp-urls"),
 ]
