1183c3c648cbe88e3de9fb5933c6468530f05f19
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-18 10:33:35 +0100

    Make migrations and add comment model

--- forumapp/models.py
+++ forumapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -12,4 +13,13 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    excerpt = models.TextField(blank=True)
+    tags = TaggableManager()
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
