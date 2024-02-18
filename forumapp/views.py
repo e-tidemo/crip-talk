949ad66fcff0cebc4b0911230d010ddf4a37c30f
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-18 11:00:42 +0100

    Add html template for a home page

--- forumapp/views.py
+++ forumapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
-def crip_talk(request):
-    return HttpResponse("Hello, forum!")
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
--- forumproject/urls.py
+++ forumproject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from forumapp.views import crip_talk
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("", include("forumapp.urls"), name="forumapp-urls"),
     path('admin/', admin.site.urls),
-    path('forum/', crip_talk, name='forum')
 ]
