a78bab0c206ddaf313ef1540a188ccac21450f46
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-18 10:42:00 +0100

    Add class Meta and def __str__ to both models

--- forumapp/models.py
+++ forumapp/models.py
@@ -17,9 +17,21 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     tags = TaggableManager()
 
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} | by {self.author}"
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.body} | by {self.author}"
