ab5766b721bbe73e5f51e4d1309b2887a1a49f10
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-19 10:05:39 +0100

    Add automatic excerpt to posts

--- forumapp/models.py
+++ forumapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.utils.html import strip_tags
 
 # Create your models here.
 
@@ -22,6 +23,12 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.title} | by {self.author}"
+    
+    def save(self, *args, **kwargs):
+        # Calculate excerpt from the first 300 characters of the content
+        self.excerpt = strip_tags(self.content[:200])
+        
+        super().save(*args, **kwargs)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
