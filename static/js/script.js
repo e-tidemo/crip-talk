88f98d9d27c522fbcbf2687aa173adabdbcc78ad
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-20 11:57:44 +0100

    Troubleshooting why burger menu doesn't work on smaller screens. No conclusions

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,13 @@
-<script>
-    // Hover effect for dropdown navbar
-    document.getElementById('topicsDropdown').addEventListener('mouseenter', function () {
-        this.querySelector('.dropdown-menu').classList.add('show') 
-        });
+// Hover effect for dropdown navbar
+document.getElementById('topicsDropdown').addEventListener('mouseenter', function () {
+    new bootstrap.Dropdown(this.querySelector('.dropdown-toggle')).show();
+});
 
-    document.getElementById('topicsDropdown').addEventListener('mouseleave', function () {
-        this.querySelector('.dropdown-menu').classList.remove('show') 
-        });
-</script>
+document.getElementById('topicsDropdown').addEventListener('mouseleave', function () {
+    new bootstrap.Dropdown(this.querySelector('.dropdown-toggle')).hide();
+});
+
+// Handle click to prevent navigation
+document.getElementById('topicsDropdown').addEventListener('click', function (event) {
+    event.preventDefault();
+});
