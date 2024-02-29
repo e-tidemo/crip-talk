2528fa22de2bd1c46b0e8dc02e8645eb6b896bc9
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-29 14:37:53 +0100

    Update code to confirm delete comment/post

--- static/js/script.js
+++ static/js/script.js
@@ -6,33 +6,6 @@ function setConfirmationModal(title, message, confirmAction) {
 }
 
 // When the delete button is clicked, set modal content and show the modal
-$('.delete-post-button').click(function () {
-    var title = 'Delete Post';
-    var message = 'Are you sure you want to delete this post?';
-    setConfirmationModal(title, message, 'delete-post');
-    $('#confirmationModal').modal('show');
-});
-
-$('.delete-comment-button').click(function () {
-    var title = 'Delete Comment';
-    var message = 'Are you sure you want to delete this comment?';
-    setConfirmationModal(title, message, 'delete-comment');
-    $('#confirmationModal').modal('show');
-});
-
-// When the modal delete button is clicked, execute the appropriate action
-$('#confirmAction').click(function () {
-    var action = $('#confirmAction').data('action');
-    if (action === 'delete-post') {
-        // Execute delete post action
-        $('#deletePostForm').submit();
-    } else if (action === 'delete-comment') {
-        // Execute delete comment action
-        $('#deleteCommentForm').submit();
-    }
-});
-
-// When the delete button is clicked, set modal content and show the modal
 $('.delete-post-button').click(function (e) {
     e.preventDefault(); // Prevent the default action of the button
     var title = 'Delete Post';
@@ -49,14 +22,18 @@ $('.delete-comment-button').click(function (e) {
     $('#confirmationModal').modal('show');
 });
 
-// When the modal delete button is clicked, execute the appropriate action
+// Update the modal action and submit the form when the "Delete" button is clicked
 $('#confirmAction').click(function () {
     var action = $('#confirmAction').data('action');
     if (action === 'delete-post') {
-        // Execute delete post action
+        // Update the form action
+        $('#deletePostForm').attr('action', '/delete-post/' + post.slug + '/');
+        // Submit the form
         $('#deletePostForm').submit();
     } else if (action === 'delete-comment') {
-        // Execute delete comment action
+        // Update the form action
+        $('#deleteCommentForm').attr('action', '/delete-comment/' + comment.id + '/');
+        // Submit the form
         $('#deleteCommentForm').submit();
     }
 });
