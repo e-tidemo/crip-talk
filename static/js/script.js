fc26afe3fef58cf3d6ca1d59b4fc291a78373387
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-29 14:22:14 +0100

    Fix bug with delete posts being deleted automatically

--- static/js/script.js
+++ static/js/script.js
@@ -31,3 +31,32 @@ $('#confirmAction').click(function () {
         $('#deleteCommentForm').submit();
     }
 });
+
+// When the delete button is clicked, set modal content and show the modal
+$('.delete-post-button').click(function (e) {
+    e.preventDefault(); // Prevent the default action of the button
+    var title = 'Delete Post';
+    var message = 'Are you sure you want to delete this post?';
+    setConfirmationModal(title, message, 'delete-post');
+    $('#confirmationModal').modal('show');
+});
+
+$('.delete-comment-button').click(function (e) {
+    e.preventDefault(); // Prevent the default action of the button
+    var title = 'Delete Comment';
+    var message = 'Are you sure you want to delete this comment?';
+    setConfirmationModal(title, message, 'delete-comment');
+    $('#confirmationModal').modal('show');
+});
+
+// When the modal delete button is clicked, execute the appropriate action
+$('#confirmAction').click(function () {
+    var action = $('#confirmAction').data('action');
+    if (action === 'delete-post') {
+        // Execute delete post action
+        $('#deletePostForm').submit();
+    } else if (action === 'delete-comment') {
+        // Execute delete comment action
+        $('#deleteCommentForm').submit();
+    }
+});
