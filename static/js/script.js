a113dfb4946c09192fbb84ef518a7033fa948fe9
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-29 13:50:46 +0100

    Add modal and add delete-pop-up to javascript file

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,34 @@
-function openReportForm() {
-    // You can customize this logic based on your design
-    document.getElementById('reportForm').style.display = 'block';
-}
+//Confirm user is sure they want to delete
+//
+$(document).ready(function () {
+    function showConfirmationModal(url, targetType) {
+        $('#confirmationModal').modal('show');
+
+        $('#confirmAction').on('click', function () {
+            // Redirect to the delete URL
+            window.location.href = url;
+        });
+
+        // Update the modal content based on the action type
+        if (targetType === 'post') {
+            $('#confirmationModalLabel').text('Delete Post');
+            $('#confirmationMessage').text('Are you sure you want to delete this post?');
+        } else if (targetType === 'comment') {
+            $('#confirmationModalLabel').text('Delete Comment');
+            $('#confirmationMessage').text('Are you sure you want to delete this comment?');
+        }
+    }
+
+    // Attach click events to delete buttons
+    $('.delete-post').on('click', function (e) {
+        e.preventDefault();
+        var deleteUrl = $(this).attr('href');
+        showConfirmationModal(deleteUrl, 'post');
+    });
+
+    $('.delete-comment').on('click', function (e) {
+        e.preventDefault();
+        var deleteUrl = $(this).attr('href');
+        showConfirmationModal(deleteUrl, 'comment');
+    });
+});
